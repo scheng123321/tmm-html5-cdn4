bba7db93b92a07518d0f43b3f1a3eeddbfef8029
Simon Cheng <simoncheng12@hotmail.com>
2021-01-11 13:33:18 +0000

    remove obsolete

--- diep/static.diep.io/pow.js
+++
@@ -1,4 +0,0 @@
-(function(){var worker=new Worker(getWorkerPath());var nextJobID=0;var workerCallbacks={};function solve(prefix,difficulty,cb){var id=nextJobID++;worker.postMessage([id,"solve",prefix,difficulty]);workerCallbacks[id]=cb;}
-worker.onmessage=function(e){var data=e.data;var id=data[0];workerCallbacks[id].apply(null,data.slice(1));}
-function getWorkerPath(){return window.location.protocol+"//"+window.location.host+window.location.pathname+"pow_worker.js";}
-window.m28=window.m28||{};window.m28.pow={solve:solve};})();
